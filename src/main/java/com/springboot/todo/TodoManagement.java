0f8a9ab05fc6b2170c643fa20df408907fbfe1dd
Darya Martyniuk <46248972+Rinot22@users.noreply.github.com>
2022-01-26 17:10:45 +0100

    main

---
+++ src/main/java/com/springboot/todo/TodoManagement.java
@@ -0,0 +1,12 @@
+package com.springboot.todo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagement {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagement.class, args);
+    }
+}
